ee08380e9e1f4a46dfb148f07ddf73e6535ebb82
vincesanityyy <vbustillo97@gmail.com>
2020-12-02 23:59:06 +0800

    updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,13 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 import * as VueGoogleMaps from 'vue2-google-maps'
-
+import toastr from 'admin-lte/plugins/toastr/toastr.min.js'
+import swal from 'admin-lte/plugins/sweetalert2/sweetalert2.min.js'
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('Home', require('./components/Home.vue').default);
+Vue.use(VueRouter)
+Vue.use(toastr)
 
 Vue.use(VueGoogleMaps, {
     load: {
